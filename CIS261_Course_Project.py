7ba8c1b17d689ff8efdc73f15e521bd696833202
username <useremail>
2023-08-27 08:20:53 -0700

    Date range

--- CIS261_Course_Project.py
+++ CIS261_Course_Project.py
@@ -1,5 +1,11 @@
+def get_date_range():
+    from_date = input("Enter the 'from' date (mm/dd/yyyy): ")
+    to_date = input("Enter the 'to' date (mm/dd/yyyy): ")
+    return from_date, to_date
+
 def get_employee_name():
-    return input("Enter employee's name: ")
+    employee_name = input("Enter Employee Name: ")
+    return employee_name
 
 def get_total_hours():
     while True:
@@ -22,63 +28,68 @@ def get_income_tax_rate():
             return tax_rate
         except ValueError:
             print("Invalid input. please enter a valid number for the income tax rate.")
-def provess_input(input_str):
-    return input_str
+
 def calculate_pay(total_hours, hourly_rate, tax_rate):
     gross_pay = total_hours * hourly_rate
-    income_tax = gross_pay * (tax_rate / 20)
+    income_tax = gross_pay * (tax_rate / 100)
     net_pay = gross_pay - income_tax
     return gross_pay, income_tax, net_pay
-def display_employee_pay(employee_name, total_hours, hourly_rate, gross_pay, income_tax_rate, income_tax, net_pay):
-    print("Employee Name:", employee_name)
-    print("Total Hours:", total_hours)
-    print("Hourly Rate:", hourly_rate)
-    print("Gross Pay:", gross_pay)
-    print("Income Tax Rate:", income_tax_rate)
-    print("Income Tax:", income_tax)
-    print("Net Pay:", net_pay)
-    print()
-def display_summary(total_hours_list, gross_pay_list, income_tax_list, net_pay_list):
-    total_employees = len(total_hours_list)
-    total_hours = sum(total_hours_list)
-    total_gross_pay = sum(gross_pay_list)
-    total_tax = sum(income_tax_list)
-    total_net_pay = sum(net_pay_list)
 
-    print("summary")
-    print("Total Employees:", total_employees)
-    print("Total Hours:", total_hours)
-    print("Total Gross Pay:", total_gross_pay)
-    print("Total Tax:", total_tax)
-    print("Total Net Pay:", total_net_pay)
-def main():
-    total_hours_list = []
-    gross_pay_list = []
-    income_tax_list = []
-    net_pay_list = []
+def printInfo(empDetailList):
+    total_employee = 0
+    total_hours = 0.00
+    total_gross_pay = 0.00
+    total_tax = 0.00
+    total_net_pay = 0.00
+    for emplist in empDetailList:
+        from_date = emplist[0]
+        to_date = emplist[1]
+        employee_name = emplist[2]
+        hours = emplist[3]
+        rate = emplist[4]
+        tax_rate = emplist[5]
+        
+        gross_pay, income_tax, net_pay = calculate_pay(hours, rate, tax_rate)
+        print(from_date, to_date, employee_name, f"{hours:,.2f}", f"{rate:,.2f}", f"{gross_pay:,.2f}", f"{tax_rate:,.1%}", f"{income_tax:,.2f}", f"{net_pay:,.2f}")
+        total_employee += 1
+        total_hours += 1
+        total_gross_pay = gross_pay
+        total_tax += income_tax
+        total_net_pay += net_pay
+        
+        empTotals["totEmp"] = total_employee
+        empTotals["totHours"] = total_hours
+        empTotals["totGross"] = total_gross_pay
+        empTotals["totTax"] = total_tax
+        empTotals["totNet"] = total_net_pay
+        
 
+def printTotals(empTotals):
+    print(f'Total Number of Employees: {empTotals["totEmp"]}')
+    print(f'Total hours of the Employee: {empTotals["totHours"]}')
+    print(f'Total Gross Pay of the Employee: {empTotals["totGross"]}')
+    print(f'Total tax of Employee: {empTotals["totTax"]}')
+    print(f'Total net pay of Employee: {empTotals["totNet"]}')
+                                                        
+if __name__ == "__main__":
+    empDetailList = []
+    empTotals = []
     while True:
         employee_name = get_employee_name()
         
-        if employee_name.lower() == "end":
-            print("Loop terminated.")
+        if (employee_name.lower() == "end"):
             break
-        total_hours = get_total_hours()
-        hourly_rate = get_hourly_rate()
-        income_tax_rate = get_income_tax_rate()
-
-        processed_hours = process_input(total_hours)
-        processed_rate = process_input(hourly_rate)
-        processed_tax_rate = process_input(income_tax_rate)
-
-        gross_pay, income_tax, net_pay, = calculate_pay(processed_hours, processed_rate, processed_tax_rate)
-
-        display_employee_pay(employee_name, processed_hours, processed_rate, gross_pay, processed_tax_rate, income_tax, net_pay)
-
-        total_hours_list.append(processed_hours)
-        gross_pay_list.append(gross_pay)
-        income_tax_list.append(income_tax)
-        net_pay_list.append(net_pay)
-    display_summary(total_hours_list, gross_pay_list, income_tax_list, net_pay_list)
-if __name__ == "__main__":
-    main()
+        from_date, to_date = get_date_range()
+        hours = get_total_hours()
+        rate = get_hourly_rate()
+        tax_rate = get_income_tax_rate()
+        empDetail = []
+        empDetail.insert(0, from_date)
+        empDetail.insert(1, to_date)
+        empDetail.insert(2, employee_name)
+        empDetail.insert(3, hours)
+        empDetail.insert(4, rate)
+        empDetail.insert(5, tax_rate)
+        empDetailList.append(empDetail)
+    printInfo(empDetailList)
+    printTotals(empTotals)
